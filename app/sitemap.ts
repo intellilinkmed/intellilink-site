66610a6e352c183620842858382f960b30215c03
intellilinkmed <info@intellilink.media>
2026-03-20 08:55:28 +0200

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://intellilink.media",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://intellilink.media/proof",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://intellilink.media/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
